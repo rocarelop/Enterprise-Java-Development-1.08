9134f0b36b2fdcf05c0ee260ada7519b0678aae2
Rocio Arellano Lopez <rocioarellano_94@hotmail.com>
2022-06-05 13:38:38 +0200

    Datos añadidos al main

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -46,20 +46,28 @@ public class Main {
 
         System.out.println("---Prueba IntArrayList---");
 
+        long startTime = System.nanoTime();
         IntArrayList listaPrueba = new IntArrayList(new ArrayList<Integer>(Arrays.asList(14, 8, 5, 17, 0, 40, 36, 11)));
         System.out.println("Imprimimos una lista de menos de 10 de capacidad: " + listaPrueba);
         listaPrueba.add(120);
         System.out.println("Añadimos un primer elemento que va rellenando la lista: " + listaPrueba);
         listaPrueba.add(2);
         listaPrueba.add(5);
-        listaPrueba.add(0);
-        listaPrueba.add(6);
+        listaPrueba.add(9);
+        listaPrueba.add(11);
+        listaPrueba.add(37);
+        listaPrueba.add(89);
+        listaPrueba.add(84);
+        listaPrueba.add(7);
+        long stopTime = System.nanoTime();
+        System.out.println(stopTime - startTime);
         System.out.println("Añadimos más elementos hasta que sobrepasa la capacidad inicial de la lista: " + listaPrueba);
         System.out.println("Comprobación del funcionamiento del get, obtenemos elemento en índice 1: " + listaPrueba.get(1));
 
 
         System.out.println("---Prueba IntVector---");
 
+        startTime = System.nanoTime();
         IntVector listaPrueba2 = new IntVector(new ArrayList<Integer>(Arrays.asList(14, 8, 5, 17, 0, 40, 36, 11)));
         System.out.println("Imprimimos una lista de menos de 20 de capacidad: " + listaPrueba2);
         listaPrueba2.add(120);
@@ -76,6 +84,8 @@ public class Main {
         listaPrueba2.add(89);
         listaPrueba2.add(84);
         listaPrueba2.add(7);
+        stopTime = System.nanoTime();
+        System.out.println(stopTime - startTime);
         System.out.println("Añadimos más elementos hasta que sobrepasa la capacidad inicial de la lista: " + listaPrueba2);
         System.out.println("Comprobación del funcionamiento del get, obtenemos elemento en índice 1: " + listaPrueba2.get(1));
 
